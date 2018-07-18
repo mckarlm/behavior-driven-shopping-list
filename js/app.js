c7f679c2aa471c16776cc6c59a80679865837102
Mckarl Manuel <manmckarl@gmail.com>
2018-07-18 13:56:33 -1000

    fixed

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-(function(){
+(function(w){
   let shoppingListInstance = new ShoppingList();
   let itemRender = shoppingListInstance.render();
 
   contentDiv = document.getElementById('content');
   contentDiv.innerHTML = itemRender;
 
-  function add_to_shopping_list(){
+  const add_to_shopping_list = function(){
     let titleInput = document.getElementsByName('title').innerHTML;
     let descriptionInput = document.getElementsByName('description').innerHTML;
     let new_shopping_list_item = new ShoppingListItem(titleInput, descriptionInput);
     shoppingListInstance.addItem(new_shopping_list_item);
   }
-  add_to_shopping_list();
-  itemRender = shoppingListInstance.render;
-})()
+  
+  w.addToShoppingList = add_to_shopping_list;
+  itemRender = shoppingListInstance.render();
+})(window)
