1969d554d71ec1000f002db955c3e11759182c74
Mckarl Manuel <manmckarl@gmail.com>
2018-07-18 18:02:07 -1000

    removeitembuttonclicked + button works, MVP status

--- js/app.js
+++ js/app.js
@@ -12,25 +12,44 @@
 
   const changeCheckedStatus = function (event) {
     let itemProperties = event.target.parentNode.outerText.split(' ');
-    let index =-1;
+    let index = -1;
 
-    for(let i=0; i < shoppingListInstance.items.length; i++){
-      if((shoppingListInstance.items[i].name === itemProperties[0]) 
-      && (shoppingListInstance.items[i].description === itemProperties[1])){
+    for (let i = 0; i < shoppingListInstance.items.length; i++) {
+      if ((shoppingListInstance.items[i].name === itemProperties[0])
+        && (shoppingListInstance.items[i].description === itemProperties[1])) {
         index = i;
       }
     }
-
-    if(index > 0){
-      if (event.target.checked) {
-        shoppingListInstance.items[index].check();
-      } else {
+    
+    if (index > -1) {
+      if (shoppingListInstance.items[index].is_done) {
         shoppingListInstance.items[index].uncheck();
+      } else {
+        shoppingListInstance.items[index].check();
       }
     }
   }
 
+  const removeItemButtonClicked = function (event) {
+    let itemProperties = event.target.parentNode.outerText.split(' ');
+    let index = -1;
+    
+    for (let i = 0; i < shoppingListInstance.items.length; i++){
+      if (shoppingListInstance.items[i].name === itemProperties[0] && shoppingListInstance.items[i].description === itemProperties[1]){
+        index = i;
+      }
+    }
+
+   
+    shoppingListInstance.removeItem(shoppingListInstance.items[index]);
+    console.log(shoppingListInstance.items)
+    contentDiv.innerHTML = shoppingListInstance.render(); 
+
+  }
+   
+
   w.add_to_shopping_list = add_to_shopping_list;
   w.changeCheckedStatus = changeCheckedStatus;
+  w.removeItemButtonClicked = removeItemButtonClicked;
 
 })(window)
--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -11,6 +11,9 @@ class ShoppingListItem {
     this.is_done = false;
   }
   render() {
-    return `<li class="completed_${this.is_done}"><span>${this.name}</span> <span>${this.description}</span><input type='checkbox' onchange='changeCheckedStatus(event)' ${this.is_done? ' checked':''}></li>`
+    return `<li class="completed_${this.is_done}"><span>${this.name}</span> 
+    <span>${this.description}</span>
+    <input type='checkbox' onchange="changeCheckedStatus(event)" ${this.is_done? ` checked`:``}>
+    <button onclick="removeItemButtonClicked(event)">X</button></li>`
   }
 }
